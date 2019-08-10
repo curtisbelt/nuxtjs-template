027845e1fb2409780d3fb11f7a2fecb8621eda0f
Curtis Belt <curtis@curtisbelt.dev>
2019-08-10 14:02:05 -0500

    chore: minor update to index - add nuxt.js logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="min-h-screen h-full bg-gray-200 flex justify-center items-center">
-    Nuxt.js Template
+  <div class="min-h-screen h-full flex justify-center items-center flex-col">
+    <img src="~assets/images/nuxtjs-logo.png" class="w-32 mb-4" />
+    <div
+      class="bg-gray-300 px-4 py-3 rounded text-base leading-none font-medium"
+    >
+      Ready to go.
+    </div>
   </div>
 </template>
 
