76df1b8d9640282a6e9b0d207128af8b18323ae8
Curtis Belt <curtis@curtisbelt.dev>
2023-06-23 15:17:54 -0400

    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   modules: [
     [
       '@nuxtjs/eslint-module',
